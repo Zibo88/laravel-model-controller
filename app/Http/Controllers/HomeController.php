f39f6f2e6ac3769f68d43c731d9f1f8cd595ea40
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-08-26 14:52:12 +0200

    ciclo foreach e collegamento css

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+// utilizzo il model includendolo nel controller
+use App\Movie;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('home');
+        // assegno una variabile alla classe MOVIE così da poter leggere l'intero database
+        $movies = Movie::all();
+        // passo i dati al $data così che siano letti poi nella pagina home
+
+        $data = [
+
+            'movies' => $movies
+        ];
+        return view('home', $data);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">">
     <title>Document</title>
 </head>
 <body>
-    <h1>sono la home</h1>
+    <h1>I 10 film più apprezzati dagli utenti</h1>
+    <ul>
+        <li>
+            {{-- stampo i risultati --}}
+            <div class="container">
+                @foreach ($movies as $movie)
+                    
+                @endforeach
+            </div>
+           
+        </li>
+    </ul>
 </body>
 </html>
