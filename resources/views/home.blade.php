9be27ebeb2f1e6f46447a28d731b87b61e434e94
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-08-26 15:24:44 +0200

    implementazione di css e stampati  gli elementi

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,20 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">">
-    <title>Document</title>
+    <title>FILMS</title>
 </head>
 <body>
     <h1>I 10 film più apprezzati dagli utenti</h1>
-    <ul>
-        <li>
             {{-- stampo i risultati --}}
             <div class="container">
-                @foreach ($movies as $movie)
-                    
-                @endforeach
+                <div class="col flex">
+                    @foreach ($movies as $movie)
+                        <div class="card">
+                            <h4 class="title">Titolo:{{$movie->title}}</h4>
+                            <div class="original_title">Titolo originale:{{$movie->original_title}}</div>
+                            <div class="nationality">Nazionalità:{{$movie->nationality}}</div>
+                            <div class="date">Data di uscita:{{$movie->date}}</div>
+                            <div class="vote">Voto:{{$movie->vote}}</div>
+                        </div>
+                    @endforeach
+                </div>
             </div>
-           
-        </li>
-    </ul>
 </body>
 </html>
